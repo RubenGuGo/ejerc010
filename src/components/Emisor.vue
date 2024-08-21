b4c66706d0c905eb0352f3329865f37e63ea6480
Curso003 <correo@correo.co>
2024-08-21 10:41:46 +0200

    funcional

---
+++ src/components/Emisor.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    data(){
+        return {
+            mensaje: 'Datos Personales',
+            nombre: '',
+            apellido: '',
+            email: ''
+        };
+    },
+    methods: {
+        enviarFormulario(){
+            this.$emit('enviar-formulario', {
+                nombre: this.nombre,
+                apellido: this.apellido,
+                email: this.email
+            });
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>{{ mensaje }}</h1>
+        <form @submit.prevent="enviarFormulario">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" v-model="nombre"/>
+
+            <label for="apellido">Apellido:</label>
+            <input type="text" id="apellido" v-model="apellido"/>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" v-model="email"/>
+
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+</template>
