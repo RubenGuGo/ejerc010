dd003584f2a881fca6c666e433ffb30a7afae903
curso10 <curso10@nomail.com>
2024-08-21 11:18:26 +0200

    test y final

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,8 +1,24 @@
 // https://on.cypress.io/api
 
-describe('My First Test', () => {
-  it('visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'You did it!')
+describe('Test de Integracion', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should fill and submit the form, then display the data in Receptor', () => {
+    // Fill the form
+    cy.get('#nombre').type('John')
+    cy.get('#apellido').type('Doe')
+    cy.get('#email').type('john.doe@example.com')
+    
+    // Submit the form
+    cy.get('form').submit()
+    
+    // Check the displayed data in Receptor
+    cy.get('#Receptor').within(() => {
+      cy.contains('Nombre: John')
+      cy.contains('Apellido: Doe')
+      cy.contains('Email: john.doe@example.com')
+    })
   })
 })
