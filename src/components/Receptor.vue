60b2498fa23f9086935352d56192cddbb3243093
Curso003 <correo@correo.co>
2024-08-21 11:01:04 +0200

    mejora estetica

--- src/components/Receptor.vue
+++ src/components/Receptor.vue
@@ -10,8 +10,8 @@
   </script>
 
 <template>
+    <h1>Datos</h1>
     <div>
-      <h2>Datos</h2>
       <p><strong>Nombre:</strong> {{ data.nombre }}</p>
       <p><strong>Apellido:</strong> {{ data.apellido }}</p>
       <p><strong>Email:</strong> {{ data.email }}</p>
@@ -19,8 +19,34 @@
   </template>
 
   
-  <style scoped>
-  p {
-    margin: 5px 0;
-  }
-  </style>
+<style scoped>
+.form-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background-color: #f9f9f9;
+}
+
+h1 {
+  text-align: center;
+  color: black;
+}
+
+div {
+  display: flex;
+  flex-direction: column;
+}
+
+p {
+  padding: 5px;
+  margin-top: 5px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-top: 38px;
+  font-weight: bold;
+}
+
+</style>
